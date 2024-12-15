05d6512c0715565655f292e023f50b8dc6fb21a3
Haeun Noh <00blowup@gmail.com>
2024-12-16 02:35:43 +0900

    feature: 핵심 기능 개발 완료

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,33 @@
-import { Text, View } from "react-native";
-// 타이틀 화면
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { router } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style = {styles.container}>
+        <View style = {{ height: 100, width: 200, backgroundColor: 'grey' }}>
+            <Text>- logo -</Text>
+        </View>
+        <View style = {{ height: 200, width: 200, backgroundColor: 'grey', margin: 10 }}>
+            <Text>- portrait -</Text>
+        </View>
+        <TouchableOpacity onPress = { function() { router.navigate('game') } }
+         style = {{ height: 100, alignItems: 'center', justifyContent: 'center', width: 200, backgroundColor: 'grey' }}>
+            <Text style = {{ textAlign: 'center' }}>게임 시작</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress = { function() { router.navigate('result') } }
+         style = {{ height: 100, alignItems: 'center', justifyContent: 'center', width: 200, backgroundColor: 'grey' }}>
+            <Text style = {{ textAlign: 'center' }}>임시</Text>
+        </TouchableOpacity>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+        }
+    });
